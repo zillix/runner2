72cd1bed9d5689bd33eb6a2484c95f6af83226b7
Alex Erlandson <alexjerlandson@gmail.com>
2014-11-20 22:02:21 -0800

    Revert "Revert "Quick HUD changes""
    This reverts commit 2bb24e45321532d0a3547531f3db05f6416a93fd.

---
+++ core/src/com/zillix/game/renderers/UIRenderer.java
@@ -0,0 +1,32 @@
+package com.zillix.game.renderers;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.zillix.game.Level;
+
+public class UIRenderer implements IRenderer {
+
+	public UIRenderer(Level level)
+	{
+	
+	}
+	
+	@Override
+	public void render(SpriteBatch batch, OrthographicCamera camera, float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
